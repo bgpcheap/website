69b2c709ca2b323f46e7b84453bb623855b18dde
Kjartan Hrafnkelsson <kjartan@kjartan.io>
2023-09-29 00:34:58 +0000

    refactor ui/ux (#5)
    * refactor ui/ux
    
    * v.ps has AS3204

--- components/IRRPopover.tsx
+++ components/IRRPopover.tsx
@@ -21,7 +21,7 @@ const IRRPopover: React.FC<IRRPopoverProps> = ({ supported }) => {
         </span>
       ) || (
           <span onMouseEnter={() => setVisible(true)}
-            onMouseLeave={() => setVisible(false)} className="inline-flex items-center rounded-md bg-orange-50 px-2 py-1 text-xs font-medium text-orange-700 ring-1 ring-inset ring-green-600/20">
+            onMouseLeave={() => setVisible(false)} className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-green-600/20">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
--- components/Providers.tsx
+++ components/Providers.tsx
@@ -54,8 +54,8 @@ const data: ProviderData[] = [
     },
     {
       "provider": {
-        "logo": "/img/ifog-gmbh.png",
-        "logo_include_text": false,
+        "logo": "/img/ifog.png",
+        "logo_include_text": true,
         "name": "iFog GmbH",
         "asn": 34927
       },
@@ -160,7 +160,7 @@ const data: ProviderData[] = [
         "logo": "https://v.ps/assets/img/logo_dark.7d4c766e.svg",
         "logo_include_text": true,
         "name": "V.ps",
-        "asn": 3214
+        "asn": 3204
       },
       "locations": ["London, GB", "Frankfurt, DE"],
       "services": ["VPS", "IXP Access"],
